42024fbe251bfe15da0b5778107a77e4f5d59826
Heman Sakkthivel M S <sakkthivelheman@gmail.com>
2023-06-20 12:06:50 +0530

    Added Download functionality

--- src/pages/certi.jsx
+++ src/pages/certi.jsx
@@ -1,8 +1,26 @@
-function Certificate() {
+import React from 'react';
+import '../index.css';
+
+  const Certificate = () => {
+    const certPath = '../../public/certificate/dummy.pdf';
+    const certName = 'certificate.pdf';
+  
+    const handleDownload = () => {
+      const link = document.createElement('a');
+      link.href = certPath;
+      link.download = certName;
+      link.click();
+    };
+  
     return (
-      <p>certi component</p>
-    )
-  }
+      <div className='cert'>
+        <h1 className='cert-header'>Name of Event for which certificate is generated</h1>
+        <button className='download-btn' onClick={handleDownload}>
+          Download Certificate
+        </button>
+      </div>
+    );
+  };
   
   export default Certificate;
   
