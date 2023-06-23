b8acc711f782b7f46574a1706706bee11992561c
JustinJiji <justinjiji002@gmail.com>
2023-06-23 15:12:23 +0530

    my change ui

--- src/pages/certi.jsx
+++ src/pages/certi.jsx
@@ -1,44 +1,71 @@
-import React from 'react';
-import '../index.css';
-import { useParams } from 'react-router-dom';
-import addToProfile from '../utils/button-add-to-profile';
-
+import React, { useState } from "react";
+import "../index.css";
+import { useParams } from "react-router-dom";
+import addToProfile from "../utils/button-add-to-profile";
+import handleDownload from "../utils/button-download";
+import { LinkedinIcon } from "react-share";
 
 const Certificate = () => {
+  const [isHovered, setIsHovered] = useState(false);
 
-  const certPath = '../../public/certificate/dummy.pdf';
-  const certName = 'certificate.pdf';
-
-  const handleDownload = () => {
-    const link = document.createElement('a');
-    link.href = certPath;
-    link.download = certName;
-    link.click();
+  const MouseEnter = () => {
+    setIsHovered(true);
   };
 
+  const MouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const bgStyle = isHovered ? { fill: "#007FB1" } : { fill: "white" };
+  const iconFillColor = isHovered ? "white" : "#007FB1";
+  const newClass = isHovered
+    ? "btn btn-transparent-hover"
+    : "btn btn-transparent";
+
   let { id } = useParams();
-      return (
-    <div className='cert'>
+
+  return (
+    <div className="cert">
       {/* Testing purpose */}
       Now showing post {id}
-      
-      <h1 className='cert-header'>Name of Event for which certificate is generated</h1>
       <div className="grid-container">
+        <h1 className="cert-header">
+          Name of Event for which certificate is generated
+        </h1>
         <div className="image-container">
-          <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--HIr7O5M4--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/z84rx7n3lfrcq62aesg0.png" alt="Your Cert" />
+          <img
+            src="https://res.cloudinary.com/practicaldev/image/fetch/s--HIr7O5M4--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/z84rx7n3lfrcq62aesg0.png"
+            alt="Your Cert"
+          />
         </div>
+
         <div class="button-container">
-          <div className="button" onClick={handleDownload} >Download Certificate</div>
-          <div className="button">Share Via LinkedIn</div>
-          <div className="button" onClick={addToProfile}>Add to LinkedIn Profile</div>
-          <div className="button">Copy Link</div>
+          <button className="btn btn-green" onClick={handleDownload}>
+            Download
+          </button>
+          <button
+            className={newClass}
+            onClick={addToProfile}
+            onMouseEnter={MouseEnter}
+            onMouseLeave={MouseLeave}
+          >
+            <LinkedinIcon
+              size={32}
+              bgStyle={bgStyle}
+              iconFillColor={iconFillColor}
+            />
+            Add to profile
+          </button>
+          <div className="separator-line"></div>
+          <div className="copy-textbox">
+            <div className="textbox">
+              <span className="text">{window.location.href}</span>
+            </div>
+            <button className="copy-btn">Copy</button>
+          </div>
         </div>
       </div>
     </div>
   );
-
-  }
-  export default Certificate;
-    
-
-  
+};
+export default Certificate;
